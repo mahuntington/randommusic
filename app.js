ca853a15ad37cfbf647d1422896a20c55b8c2104
Matt Huntington <matt.huntington@gmail.com>
2020-07-27 07:18:10 -0400

    multiple measures

--- app.js
+++ app.js
@@ -10,31 +10,36 @@ renderer.resize(1000, 1000);
 // And get a drawing context:
 var context = renderer.getContext();
 
-// Create a stave at position 10, 40 of width 400 on the canvas.
-var stave = new VF.Stave(10, 40, 800);
 
-// Add a clef and time signature.
-stave.addClef("treble").addTimeSignature("7/8");
+const drawNotes = (measureNum) => {
+    // Create a stave at position 10, 40 of width 400 on the canvas.
+    var stave = new VF.Stave(10+(measureNum*400), 40, 400);
 
-// Connect it to the rendering context and draw!
-stave.setContext(context).draw();
+    // Add a clef and time signature.
+    if(measureNum === 0){
+        stave.addClef("treble").addTimeSignature("4/4");
+    }
 
-var notes = [
-  new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "8" }),
-  new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "8" }),
-];
+    // Connect it to the rendering context and draw!
+    stave.setContext(context).draw();
 
-// Create a voice in 4/4 and add the notes from above
-var voice = new VF.Voice({num_beats: 7,  beat_value: 8});
-voice.addTickables(notes);
+    var notes = [
+      new VF.StaveNote({clef: "treble", keys: ["c/4"], duration: "4" }),
+      new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "4" }),
+      new VF.StaveNote({clef: "treble", keys: ["b/4"], duration: "4" }),
+      new VF.StaveNote({clef: "treble", keys: ["d/4"], duration: "4" }),
+    ];
 
-// Format and justify the notes to 400 pixels.
-var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+    // Create a voice in 4/4 and add the notes from above
+    var voice = new VF.Voice({num_beats: 4,  beat_value: 4});
+    voice.addTickables(notes);
 
-// Render voice
-voice.draw(context, stave);
+    // Format and justify the notes to 400 pixels.
+    var formatter = new VF.Formatter().joinVoices([voice]).format([voice], 400);
+
+    // Render voice
+    voice.draw(context, stave);
+}
+
+drawNotes(0);
+drawNotes(1);
